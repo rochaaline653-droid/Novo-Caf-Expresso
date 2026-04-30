0b7dcebfc0172687744594cab9d9f716a67375e0
alinerocha <rochaaline653@gmail.com>
2026-04-30 14:24:01 -0300

    Novo comentario no construtor

--- src/main/java/cafeexpresso/Produto.java
+++ src/main/java/cafeexpresso/Produto.java
@@ -3,7 +3,7 @@ public class Produto {
     private String nome;
     private double preco;
 
-    //Construtor
+    //Construtor de produtos
     public Produto(String nome, double preco) {
         this.nome = nome;
         this.preco = preco;
