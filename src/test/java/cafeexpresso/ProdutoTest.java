ff039312031f6474063858b8402fb04cb0ce1c69
alinerocha <rochaaline653@gmail.com>
2026-05-06 22:06:51 -0300

    Removi o import satatic org.junit.jupiter.api.Assertions.assertThrows; por não está utilisando ele.

--- src/test/java/cafeexpresso/ProdutoTest.java
+++ src/test/java/cafeexpresso/ProdutoTest.java
@@ -1,6 +1,5 @@
 package cafeexpresso;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class ProdutoTest {
